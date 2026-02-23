d2531fa0fc396fdc235712227497d58dac6327b4
ILhankhondaker <ilhankhondaker@gmail.com>
2026-02-23 11:18:45 +0600

    change logo

--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -38,7 +38,7 @@ const Navbar = () => {
             >
               <Image
                 className="object-cover w-full h-full"
-                src={"/images/logo.png"}
+                src={"/images/logo.svg"}
                 alt="Logo"
                 width={100}
                 height={100}
